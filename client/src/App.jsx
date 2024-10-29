ce64d2c87518c8453264ad19e5beede1702fb66e
Donald Machon <45331565+naldmach@users.noreply.github.com>
2024-10-29 10:21:15 +0800

    chore: create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 export default function App() {
-  return (
-    <h1>InCa Homes</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+  </Routes>
+  </BrowserRouter>
 }
